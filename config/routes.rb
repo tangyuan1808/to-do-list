6b7530833519714dd09ea72fe2a72f56d912b698
Liang Zhang <lzhang@vailsys.com>
2019-06-14 15:52:53 -0500

    Set up session controller for login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  root to: 'sessions#new'
+
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'home', to: 'home#index'
 end
