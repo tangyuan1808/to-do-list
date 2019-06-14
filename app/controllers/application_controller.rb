e83eb91c3d78e6c9ebe0bd5412339dcc3517060a
Liang Zhang <lzhang@vailsys.com>
2019-06-14 16:10:55 -0500

    Force authentication on all pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
   helper_method :current_user
+
+  def authenticate_user!
+    unless current_user
+      redirect_to root_path
+    end
+  end
 
   def current_user
     if session[:user_id]
